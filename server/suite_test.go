29619baabbfdcc024b4c7c165bd65f812233e822
Michael Parker <michael@parker.gg>
2020-11-09 17:16:29 -0600

    consistent package naming

--- server/suite_test.go
+++ server/suite_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/brianvoe/gofakeit/v5"
 	"github.com/golang/protobuf/ptypes/empty"
-	grafeas "github.com/liatrio/rode-api/protodeps/grafeas/proto/v1beta1/grafeas_go_proto"
+	grafeas_proto "github.com/liatrio/rode-api/protodeps/grafeas/proto/v1beta1/grafeas_go_proto"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"go.uber.org/zap"
@@ -26,27 +26,27 @@ var _ = BeforeSuite(func() {
 })
 
 type mockGrafeasClient struct {
-	receivedBatchCreateOccurrenceRequest  *grafeas.BatchCreateOccurrencesRequest
-	preparedBatchCreateOccurrenceResponse *grafeas.BatchCreateOccurrencesResponse
+	receivedBatchCreateOccurrenceRequest  *grafeas_proto.BatchCreateOccurrencesRequest
+	preparedBatchCreateOccurrenceResponse *grafeas_proto.BatchCreateOccurrencesResponse
 }
 
-func (c *mockGrafeasClient) GetOccurrence(context.Context, *grafeas.GetOccurrenceRequest, ...grpc.CallOption) (*grafeas.Occurrence, error) {
+func (c *mockGrafeasClient) GetOccurrence(context.Context, *grafeas_proto.GetOccurrenceRequest, ...grpc.CallOption) (*grafeas_proto.Occurrence, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) ListOccurrences(context.Context, *grafeas.ListOccurrencesRequest, ...grpc.CallOption) (*grafeas.ListOccurrencesResponse, error) {
+func (c *mockGrafeasClient) ListOccurrences(context.Context, *grafeas_proto.ListOccurrencesRequest, ...grpc.CallOption) (*grafeas_proto.ListOccurrencesResponse, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) DeleteOccurrence(context.Context, *grafeas.DeleteOccurrenceRequest, ...grpc.CallOption) (*empty.Empty, error) {
+func (c *mockGrafeasClient) DeleteOccurrence(context.Context, *grafeas_proto.DeleteOccurrenceRequest, ...grpc.CallOption) (*empty.Empty, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) CreateOccurrence(context.Context, *grafeas.CreateOccurrenceRequest, ...grpc.CallOption) (*grafeas.Occurrence, error) {
+func (c *mockGrafeasClient) CreateOccurrence(context.Context, *grafeas_proto.CreateOccurrenceRequest, ...grpc.CallOption) (*grafeas_proto.Occurrence, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) BatchCreateOccurrences(ctx context.Context, req *grafeas.BatchCreateOccurrencesRequest, opt ...grpc.CallOption) (*grafeas.BatchCreateOccurrencesResponse, error) {
+func (c *mockGrafeasClient) BatchCreateOccurrences(ctx context.Context, req *grafeas_proto.BatchCreateOccurrencesRequest, opt ...grpc.CallOption) (*grafeas_proto.BatchCreateOccurrencesResponse, error) {
 	c.receivedBatchCreateOccurrenceRequest = req
 
 	// if we have a prepared response, send it. otherwise, return nil
@@ -57,42 +57,42 @@ func (c *mockGrafeasClient) BatchCreateOccurrences(ctx context.Context, req *gra
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) UpdateOccurrence(context.Context, *grafeas.UpdateOccurrenceRequest, ...grpc.CallOption) (*grafeas.Occurrence, error) {
+func (c *mockGrafeasClient) UpdateOccurrence(context.Context, *grafeas_proto.UpdateOccurrenceRequest, ...grpc.CallOption) (*grafeas_proto.Occurrence, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) GetOccurrenceNote(context.Context, *grafeas.GetOccurrenceNoteRequest, ...grpc.CallOption) (*grafeas.Note, error) {
+func (c *mockGrafeasClient) GetOccurrenceNote(context.Context, *grafeas_proto.GetOccurrenceNoteRequest, ...grpc.CallOption) (*grafeas_proto.Note, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) GetNote(context.Context, *grafeas.GetNoteRequest, ...grpc.CallOption) (*grafeas.Note, error) {
+func (c *mockGrafeasClient) GetNote(context.Context, *grafeas_proto.GetNoteRequest, ...grpc.CallOption) (*grafeas_proto.Note, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) ListNotes(context.Context, *grafeas.ListNotesRequest, ...grpc.CallOption) (*grafeas.ListNotesResponse, error) {
+func (c *mockGrafeasClient) ListNotes(context.Context, *grafeas_proto.ListNotesRequest, ...grpc.CallOption) (*grafeas_proto.ListNotesResponse, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) DeleteNote(context.Context, *grafeas.DeleteNoteRequest, ...grpc.CallOption) (*empty.Empty, error) {
+func (c *mockGrafeasClient) DeleteNote(context.Context, *grafeas_proto.DeleteNoteRequest, ...grpc.CallOption) (*empty.Empty, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) CreateNote(context.Context, *grafeas.CreateNoteRequest, ...grpc.CallOption) (*grafeas.Note, error) {
+func (c *mockGrafeasClient) CreateNote(context.Context, *grafeas_proto.CreateNoteRequest, ...grpc.CallOption) (*grafeas_proto.Note, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) BatchCreateNotes(context.Context, *grafeas.BatchCreateNotesRequest, ...grpc.CallOption) (*grafeas.BatchCreateNotesResponse, error) {
+func (c *mockGrafeasClient) BatchCreateNotes(context.Context, *grafeas_proto.BatchCreateNotesRequest, ...grpc.CallOption) (*grafeas_proto.BatchCreateNotesResponse, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) UpdateNote(context.Context, *grafeas.UpdateNoteRequest, ...grpc.CallOption) (*grafeas.Note, error) {
+func (c *mockGrafeasClient) UpdateNote(context.Context, *grafeas_proto.UpdateNoteRequest, ...grpc.CallOption) (*grafeas_proto.Note, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) ListNoteOccurrences(context.Context, *grafeas.ListNoteOccurrencesRequest, ...grpc.CallOption) (*grafeas.ListNoteOccurrencesResponse, error) {
+func (c *mockGrafeasClient) ListNoteOccurrences(context.Context, *grafeas_proto.ListNoteOccurrencesRequest, ...grpc.CallOption) (*grafeas_proto.ListNoteOccurrencesResponse, error) {
 	return nil, nil
 }
 
-func (c *mockGrafeasClient) GetVulnerabilityOccurrencesSummary(context.Context, *grafeas.GetVulnerabilityOccurrencesSummaryRequest, ...grpc.CallOption) (*grafeas.VulnerabilityOccurrencesSummary, error) {
+func (c *mockGrafeasClient) GetVulnerabilityOccurrencesSummary(context.Context, *grafeas_proto.GetVulnerabilityOccurrencesSummaryRequest, ...grpc.CallOption) (*grafeas_proto.VulnerabilityOccurrencesSummary, error) {
 	return nil, nil
 }
