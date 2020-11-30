a4da774fcd5c0ad23ebf0daff482251cc67dc47e
Chris Schreiber <chriss@liatrio.com>
2020-11-30 12:31:35 -0800

    add attest policy method to protobuf

--- proto/v1alpha1/rode_grpc.pb.go
+++ proto/v1alpha1/rode_grpc.pb.go
@@ -4,6 +4,7 @@ package v1alpha1
 
 import (
 	context "context"
+	v1alpha1 "github.com/liatrio/rode-api/proto/v1alpha1"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -18,6 +19,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type RodeClient interface {
 	BatchCreateOccurrences(ctx context.Context, in *BatchCreateOccurrencesRequest, opts ...grpc.CallOption) (*BatchCreateOccurrencesResponse, error)
+	AttestPolicy(ctx context.Context, in *v1alpha1.AttestPolicyRequest, opts ...grpc.CallOption) (*v1alpha1.AttestPolicyResponse, error)
 }
 
 type rodeClient struct {
@@ -37,11 +39,21 @@ func (c *rodeClient) BatchCreateOccurrences(ctx context.Context, in *BatchCreate
 	return out, nil
 }
 
+func (c *rodeClient) AttestPolicy(ctx context.Context, in *v1alpha1.AttestPolicyRequest, opts ...grpc.CallOption) (*v1alpha1.AttestPolicyResponse, error) {
+	out := new(v1alpha1.AttestPolicyResponse)
+	err := c.cc.Invoke(ctx, "/rode.v1alpha1.Rode/AttestPolicy", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // RodeServer is the server API for Rode service.
 // All implementations must embed UnimplementedRodeServer
 // for forward compatibility
 type RodeServer interface {
 	BatchCreateOccurrences(context.Context, *BatchCreateOccurrencesRequest) (*BatchCreateOccurrencesResponse, error)
+	AttestPolicy(context.Context, *v1alpha1.AttestPolicyRequest) (*v1alpha1.AttestPolicyResponse, error)
 	mustEmbedUnimplementedRodeServer()
 }
 
@@ -51,6 +63,9 @@ type UnimplementedRodeServer struct {
 
 func (UnimplementedRodeServer) BatchCreateOccurrences(context.Context, *BatchCreateOccurrencesRequest) (*BatchCreateOccurrencesResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method BatchCreateOccurrences not implemented")
+}
+func (UnimplementedRodeServer) AttestPolicy(context.Context, *v1alpha1.AttestPolicyRequest) (*v1alpha1.AttestPolicyResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AttestPolicy not implemented")
 }
 func (UnimplementedRodeServer) mustEmbedUnimplementedRodeServer() {}
 
@@ -83,6 +98,24 @@ func _Rode_BatchCreateOccurrences_Handler(srv interface{}, ctx context.Context, 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Rode_AttestPolicy_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(v1alpha1.AttestPolicyRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RodeServer).AttestPolicy(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/rode.v1alpha1.Rode/AttestPolicy",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RodeServer).AttestPolicy(ctx, req.(*v1alpha1.AttestPolicyRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Rode_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "rode.v1alpha1.Rode",
 	HandlerType: (*RodeServer)(nil),
@@ -90,6 +123,10 @@ var _Rode_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "BatchCreateOccurrences",
 			Handler:    _Rode_BatchCreateOccurrences_Handler,
+		},
+		{
+			MethodName: "AttestPolicy",
+			Handler:    _Rode_AttestPolicy_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
