31de0e72ff1673afcb474ce80974439f98e072b7
Alex Ashley <alexa@liatrio.com>
2021-04-07 14:37:31 -0500

    Add utility for parsing resource URIs

---
+++ server/util.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	resourceUriPatterns = []*regexp.Regexp{
+		// Docker images
+		regexp.MustCompile("(?P<name>.+)(@sha256:)(?P<version>.+)"),
+		// Git repositories
+		regexp.MustCompile("^(git:/{2})(?P<name>.+)@(?P<version>.+)"),
+		// Maven packages
+		regexp.MustCompile("^(gav:/{2})(?P<name>.+):(?P<version>.+)"),
+		// Files
+		regexp.MustCompile("^(file:/{2}sha256:)(?P<version>.+):(?P<name>.+)"),
+		// NPM packages
+		regexp.MustCompile("^(npm:/{2})(?P<name>.+):(?P<version>.+)"),
+		// NuGet packages
+		regexp.MustCompile("^(nuget:/{2})(?P<name>.+):(?P<version>.+)"),
+		// pip packages
+		regexp.MustCompile("^(pip:/{2})(?P<name>.+):(?P<version>.+)"),
+		// Debian packages
+		regexp.MustCompile("^(deb:/{2}).*:(?P<name>.+):(?P<version>.+)"),
+		// RPM packages
+		regexp.MustCompile("^(rpm:/{2}).*:(?P<name>.+):(?P<version>.+)"),
+	}
+)
+
+type resourceUriComponents struct {
+	name    string
+	version string
+}
+
+func parseResourceUri(uri string) (*resourceUriComponents, error) {
+	var resourceRegex *regexp.Regexp
+
+	for _, pattern := range resourceUriPatterns {
+		if pattern.MatchString(uri) {
+			resourceRegex = pattern
+			break
+		}
+	}
+
+	if resourceRegex == nil {
+		return nil, fmt.Errorf("unable to determine resource type for uri: %s", uri)
+	}
+
+	matches := resourceRegex.FindStringSubmatch(uri)
+	name := matches[resourceRegex.SubexpIndex("name")]
+	version := matches[resourceRegex.SubexpIndex("version")]
+
+	return &resourceUriComponents{name, version}, nil
+}
---
+++ server/util_test.go
@@ -0,0 +1,84 @@
+package server
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("util", func() {
+	Describe("parseResourceUri", func() {
+		DescribeTable("valid resource types", func(resourceUri string, expected *resourceUriComponents) {
+			actual, err := parseResourceUri(resourceUri)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(actual).To(Equal(expected))
+		},
+			Entry("Docker image", "https://gcr.io/scanning-customer/dockerimage@sha256:244fd47e07d1004f0aed9c156aa09083c82bf8944eceb67c946ff7430510a77b", &resourceUriComponents{
+				name:    "https://gcr.io/scanning-customer/dockerimage",
+				version: "244fd47e07d1004f0aed9c156aa09083c82bf8944eceb67c946ff7430510a77b",
+			}),
+			Entry("Debian package", "deb://lucid:i386:acl:2.2.49-2", &resourceUriComponents{
+				name:    "acl",
+				version: "2.2.49-2",
+			}),
+			Entry("Debian package without a specified distribution", "deb://arm64:build-essential:12.9", &resourceUriComponents{
+				name:    "build-essential",
+				version: "12.9",
+			}),
+			Entry("Generic file", "file://sha256:244fd47e07d1004f0aed9c156aa09083c82bf8944eceb67c946ff7430510a77b:foo.jar", &resourceUriComponents{
+				name:    "foo.jar",
+				version: "244fd47e07d1004f0aed9c156aa09083c82bf8944eceb67c946ff7430510a77b",
+			}),
+			Entry("Maven package", "gav://ant:ant:1.6.5", &resourceUriComponents{
+				name:    "ant:ant",
+				version: "1.6.5",
+			}),
+			Entry("npm package", "npm://mocha:2.4.5", &resourceUriComponents{
+				name:    "mocha",
+				version: "2.4.5",
+			}),
+			Entry("scoped npm package", "npm://@babel/core:7.13.14", &resourceUriComponents{
+				name:    "@babel/core",
+				version: "7.13.14",
+			}),
+			Entry("NuGet", "nuget://log4net:9.0.1", &resourceUriComponents{
+				name:    "log4net",
+				version: "9.0.1",
+			}),
+			Entry("pip package", "pip://raven:5.13.0", &resourceUriComponents{
+				name:    "raven",
+				version: "5.13.0",
+			}),
+			Entry("RPM package", "rpm://el6:i386:ImageMagick:6.7.2.7-4", &resourceUriComponents{
+				name:    "ImageMagick",
+				version: "6.7.2.7-4",
+			}),
+			Entry("RPM without a specified distribution", "rpm://el6:i386:ImageMagick:6.7.2.7-4", &resourceUriComponents{
+				name:    "ImageMagick",
+				version: "6.7.2.7-4",
+			}),
+			Entry("Git repository (GitHub)", "git://github.com/rode/rode@bca0e1b89be42a61131b6de09fd2836e7b00c252", &resourceUriComponents{
+				name:    "github.com/rode/rode",
+				version: "bca0e1b89be42a61131b6de09fd2836e7b00c252",
+			}),
+			Entry("Git repository (Azure DevOps)", "git://dev.azure.com/rode/rode/_git/rode@bca0e1b89be42a61131b6de09fd2836e7b00c252", &resourceUriComponents{
+				name:    "dev.azure.com/rode/rode/_git/rode",
+				version: "bca0e1b89be42a61131b6de09fd2836e7b00c252",
+			}),
+		)
+
+		Describe("invalid resource uris", func() {
+			When("a resource uri contains an unexpected type", func() {
+				It("should return an error", func() {
+					invalidUri := "foo://bar"
+					actual, err := parseResourceUri(invalidUri)
+
+					Expect(actual).To(BeNil())
+					Expect(err).To(MatchError("unable to determine resource type for uri: " + invalidUri))
+				})
+			})
+		})
+	})
+})
