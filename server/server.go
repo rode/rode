1a5d9523a5074900ac70a547f92575f9d8dfa325
JonRudy <jonr@liatrio.com>
2021-04-28 13:27:16 -0700

    modified mappings to include properties for fields to be sorted on

--- server/server.go
+++ server/server.go
@@ -358,6 +358,11 @@ func (r *rodeServer) ListGenericResources(ctx context.Context, request *pb.ListG
 	}, nil
 }
 
+type indexSetting struct {
+	index      string
+	properties map[string]interface{}
+}
+
 func (r *rodeServer) initialize(ctx context.Context) error {
 	log := r.logger.Named("initialize")
 
@@ -376,9 +381,23 @@ func (r *rodeServer) initialize(ctx context.Context) error {
 		}
 	}
 
-	for _, index := range []string{rodeElasticsearchPoliciesIndex, rodeElasticsearchGenericResourcesIndex} {
-		if err := r.createIndex(ctx, index); err != nil {
-			return fmt.Errorf("error creating index %s: %s", index, err)
+	indexSettings := []indexSetting{
+		{index: rodeElasticsearchPoliciesIndex, properties: map[string]interface{}{
+			"created": map[string]interface{}{
+				"type": "date",
+			},
+		}},
+		{index: rodeElasticsearchGenericResourcesIndex, properties: map[string]interface{}{
+			"name": map[string]interface{}{
+				"type": "keyword",
+			},
+		}},
+	}
+
+	for _, intSettings := range indexSettings {
+		if err := r.createIndex(ctx, intSettings); err != nil {
+
+			return fmt.Errorf("error creating index %s: %s", intSettings, err)
 		}
 	}
 
@@ -704,12 +723,13 @@ func (r *rodeServer) UpdatePolicy(ctx context.Context, updatePolicyRequest *pb.U
 	return policy, nil
 }
 
-func (r *rodeServer) createIndex(ctx context.Context, indexName string) error {
+func (r *rodeServer) createIndex(ctx context.Context, settings indexSetting) error {
 	mappings := map[string]interface{}{
 		"mappings": map[string]interface{}{
 			"_meta": map[string]interface{}{
 				"type": "rode",
 			},
+			"properties": settings.properties,
 			"dynamic_templates": []map[string]interface{}{
 				{
 					"strings_as_keywords": map[string]interface{}{
@@ -724,7 +744,7 @@ func (r *rodeServer) createIndex(ctx context.Context, indexName string) error {
 		},
 	}
 	body, _ := esutil.EncodeRequest(mappings)
-	response, err := r.esClient.Indices.Create(indexName, r.esClient.Indices.Create.WithBody(body), r.esClient.Indices.Create.WithContext(ctx))
+	response, err := r.esClient.Indices.Create(settings.index, r.esClient.Indices.Create.WithBody(body), r.esClient.Indices.Create.WithContext(ctx))
 
 	if err != nil {
 		return err
