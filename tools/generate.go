f23e30b92ca59f00630345b631c71d813d4d10a5
Alex Ashley <alexa@liatrio.com>
2021-06-14 15:06:13 -0400

    Prevent changes to go.mod by using go install for `tools/`

--- tools/generate.go
+++ tools/generate.go
@@ -14,7 +14,7 @@
 
 // This file downloads dependencies that are required for working with protobufs
 
-//go:generate env GO111MODULE=on go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.25.0
-//go:generate env GO111MODULE=on go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.0.1
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.25.0
+//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.0.1
 
 package tools
