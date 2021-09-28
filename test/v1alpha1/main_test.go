4d3901176c8323544db17feda40cf62f385068f1
Alex Ashley <alexa@liatrio.com>
2021-09-28 09:21:13 -0400

    license headers

--- test/v1alpha1/main_test.go
+++ test/v1alpha1/main_test.go
@@ -1,16 +1,30 @@
+// Copyright 2021 The Rode Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package v1alpha1_test
 
 import (
 	"flag"
+	"log"
+	"os"
+	"testing"
+
 	"github.com/brianvoe/gofakeit/v6"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/rode/rode/common"
 	"github.com/rode/rode/proto/v1alpha1"
-
-	"log"
-	"os"
-	"testing"
 )
 
 var (
